d8ac5c9c81e4da02581ee022e03826fa6118f3aa
sule0 <99013587+sule0@users.noreply.github.com>
2022-10-03 15:19:30 +0300

    classDemo

---
+++ DortIslem.java
@@ -0,0 +1,29 @@
+package classDemo;
+
+public class DortIslem {
+
+	public int sum(int num1, int num2) {
+	    int sum=(num1+num2);
+	    System.out.println("sum= "+sum);
+	    return sum;
+	}
+
+	public int extraction(int num1, int num2) {
+		int  extraction=(num1-num2);
+		System.out.println("extraction= "+extraction);
+		return extraction;
+	}
+    public float divide(float num1,float num2) {
+    	
+    	float division=(num1/num2);
+    	System.out.println("division= "+division);
+    	return division;
+    }
+    
+    public int multiply(int num1,int num2) 
+    {
+    	int product=(num1*num2);
+    	System.out.println("product= "+product);
+    	return product;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+package classDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DortIslem dortIslem = new DortIslem();
+		dortIslem.multiply(2,9);
+		dortIslem.divide(10, 3);
+		dortIslem.sum(45, 34);
+		dortIslem.extraction(2, 17);
+		
+	}
+
+}
